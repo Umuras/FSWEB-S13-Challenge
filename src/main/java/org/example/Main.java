14677e9723a55d024b1ea7796cd3e08cc3e54f23
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-01-10 14:28:04 +0300

    All tests finished.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        allOperations();
+    }
+
+    public static void allOperations()
+    {
+        Healthplan healthplan = new Healthplan(1,"ALT-Sigorta", Plan.BASIC);
+        healthplan.ToString();
+        String[] healthPlans = new String[2];
+        healthPlans[0] = healthplan.getName();
+        Employee employee = new Employee(1,"Ali Umur Kucur", "auk@gmail.com","12345",healthPlans);
+        employee.ToString();
+        String[] developerNames = new String[5];
+        developerNames[0] = "Ali Umur";
+        developerNames[1] = "Oğuz";
+        developerNames[2] = "Mustafa";
+        developerNames[3] = "Hilal";
+        developerNames[4] = "Asım";
+        Company company = new Company(1,"AUK Software",100000, developerNames);
+        company.ToString();
     }
 }
